ecd9986b8bca4a3fb8813bbefb46f7c69780afeb
Sitar <Sitar@LAPTOP-VMM1IVJ2>
2021-03-28 14:37:37 +0530

    create e2eselenium project

---
+++ E2EseleniumJenkins/src/TesttingPackage/TC_003.java
@@ -0,0 +1,22 @@
+package TesttingPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	WebDriver driver;
+	@Test
+	public void testcase_001(){
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://flipkart.com");
+
+	}
+}
---
+++ E2EseleniumJenkins/src/TesttingPackage/TestCases.java
@@ -0,0 +1,25 @@
+package TesttingPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class TestCases {
+	WebDriver driver;
+	@Test
+	public void testcase_001(){
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://fb.com");
+
+		
+	}
+
+}
